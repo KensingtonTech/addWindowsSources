972d2bb1eb66c2a01620d8ede88ae8e91d2f0a96
Tim Underhay <15734900+KensingtonTech@users.noreply.github.com>
2018-06-11 11:00:08 -0600

    Update to version 0.2

--- addWindowsSources.py
+++ addWindowsSources.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 
-version='0.1'
+version='0.2'
 
 """
 addWindowsSources.py
-28 November, 2016
+13 July, 2017
 by Tim Underhay
 Kensington Technology Associates, Limited
 tim.underhay@kensingtontechlimited.com
@@ -12,7 +12,7 @@ http://kensingtontechlimited.com
 
 MIT License
 
-Copyright (c) 2016, Kensington Technology Associates, Limited
+Copyright (c) 2017, Kensington Technology Associates, Limited
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -48,14 +48,17 @@ import urlparse
 from requests.auth import HTTPBasicAuth
 import tempfile
 import subprocess
+import json
 
-global found
-found=False
+#from pprint import pprint
+
+global grerequestsModuleFound
+grerequestsModuleFound=False
 import imp
-try: #try importing the grequests module, and set found=True if it is present. This enables async IO if module is present
+try: #try importing the grequests module, and set grerequestsModuleFound=True if it is present. This enables async IO if module is present
   imp.find_module('grequests')
   import grequests
-  found = True
+  grerequestsModuleFound = True
 except ImportError:
   pass
 
@@ -68,19 +71,20 @@ The Event Category must already be defined in the Windows Collection configurati
 All fields will be URL-encoded by this script
 *** This script will by default run in synchronous mode, which is potentially slow when adding many event sources.  Installation of the 'grequests' python module will enable asynchronous mode, which should greatly increase execution speed for large data sets.
 
-Example: addWindowsSources.py -f mycsv.csv -h logcollector -u admin -p netwitness -c Security
+Example: addWindowsSources.py -f mycsv.csv -h logcollector -u admin -p netwitness -c Security --delete
 
 CSV Format and example:
 
 eventsource_address,port_number,transport_mode,debug,enabled,cert_name,validate_server,render_locale,windows_type,resolve_sids,sids_interval,sids_timeout,override_channels
-"dc1.mydomain.local","5985","http","0","true","","false","en-US","Domain Controller","true","14400","60","" 
-"host1.mydomain.local","5985","http","0","true","","false","en-US","Non-Domain Controller","true","14400","60","" 
+dc1.mydomain.local,5985,http,0,true,,false,en-US,Domain Controller,true,14400,60, 
+host1.mydomain.local,5985,http,0,true,,false,en-US,Non-Domain Controller,true,14400,60, 
 
 Options:
   -f, --file        CSV filename
+      --delete      Delete event sources from Log Collector which are not present in the import CSV.  Matches by hostname
   -d, --url         URL to download CSV file from.  Supercedes --file
   -h, --host        Hostname of Log Collector
-  -c, --category    Windows Event Category to add event sources to
+  -c, --category    Windows Event Category (i.e. Domain) to add event sources to
   -u, --user        Username to authenticate to Log Collector with
   -p, --password    Password to authenticate to Log Collector with
   -r, --rest        REST port to connect to (optional).  Default is 50101
@@ -103,6 +107,9 @@ def parseCSV(filename):
 
   return csvD
  
+
+
+
 def downloadCSV(url):
   try:
     with tempfile.NamedTemporaryFile('wb',delete=False) as file:
@@ -119,33 +126,18 @@ def downloadCSV(url):
     print 'Fetched file from URL ' + url
   os.unlink(file.name)
   return csvD
-  
-def olddownloadCSV(url): #original CSV downloader method.  Switched to using curl to enable proper authentication.
-  try:
-    resp = urllib2.urlopen(url)
-    with tempfile.NamedTemporaryFile('wb',delete=False) as file:
-      file.write(resp.read())
-      file.close()
-      csvD=parseCSV(file.name)
-  except urllib2.URLError, e:
-    print 'ERROR ' + str(e) + ' downloading from URL ' + url
-    sys.exit(1)
-  except urllib2.HTTPError:
-    print 'HTTP Error: ' + str(e) + ' downloading from URL ' + url
-    sys.exit(1)
-  else:
-    print 'Fetched file from URL ' + url
-  os.unlink(file.name)
-  return csvD
+
+
 
 def main(argv):
 
+  deleteOld = False
   ssl=False
   global restPort
   restPort='50101' #This should work for nearly all but can still be set from CLI
 
   try:
-    opts, args = getopt.getopt(argv, 'f:d:h:c:u:p:r:s?', ['file=','url=','host=','category=','user=','password=','rest=','ssl','help'] )
+    opts, args = getopt.getopt(argv, 'f:d:h:c:u:p:r:s?', ['file=','url=','host=','delete','category=','user=','password=','rest=','ssl','help'] )
   except getopt.GetoptError:
     raise
     usage()
@@ -161,6 +153,8 @@ def main(argv):
     elif opt in ('--category', '-c'):
       global categoryName
       categoryName=arg
+    elif opt in ('--delete'):
+      deleteOld=True
     elif opt in ('-u','--user'):
       user=arg
     elif opt in ('-h','--host'):
@@ -182,23 +176,52 @@ def main(argv):
   if ssl:
     proto='https://'
 
-  csvDict=[]
+  csvList=[]
 
   if 'url' in vars() or 'url' in globals():
-    csvDict=downloadCSV(url)
+    csvList=downloadCSV(url)
   else:
-    csvDict=parseCSV(filename)
+    csvList=parseCSV(filename)
 
-  del csvDict[0] #remove first line of CSV, assuming it's the definition
+  del csvList[0] #remove first line of CSV, assuming it's the definition
   
   urlList=[]
   
-  if not found:
-    print 'Using synchronous mode.  If the CSV is large, the script could take some time to complete.  Install python module "grequests" to use asynchronous mode (but NOT if this is an SA host!!!)'
+  ###################################################################################
+  oldEventSources = [] #this list contains the existing event sources for the selected event category, pulled from the log collector
+  #print "Fetching existing event source definitions for Event Category", categoryName
+  oldEventSourceUri = '/logcollection/windows/eventsources/' + categoryName + '?msg=ls&depth=2&force-content-type=application/json&expiry=600'
+  oldEventSourcesUrl = proto + host + ':' + restPort + oldEventSourceUri
+  try:
+    oldEventSourceDefs=json.loads( getExistingEventSources(oldEventSourcesUrl, user, password) )
+    #pprint(oldEventSourceDefs)
+  except Exception as e:
+    print "ERROR parsing JSON response:",e
+    sys.exit(1)
 
+  for node in oldEventSourceDefs['nodes']:
+    if node['name'] == 'eventsource_address':
+      #print node['display']
+      oldEventSources.append(node['value'].lower())
+  #pprint(oldEventSources)
+  ###################################################################################
+  
+  #sys.exit(0)
+  
+  if not grerequestsModuleFound:
+    print 'Using synchronous mode.  If there are many new event sources to add, the script could take some time to complete.  Install python module "grequests" to use asynchronous mode (but NOT if this is an SA host!!!)'
+    
+  newEventSources = []
 
-  for entry in csvDict:
+  #add new entries to event category
+  for entry in csvList:
     eventsource_address,port_number,transport_mode,debug,enabled,cert_name,validate_server,render_locale,windows_type,resolve_sids,sids_interval,sids_timeout,override_channels = entry
+
+    newEventSources.append(eventsource_address)
+
+    if eventsource_address.lower() in oldEventSources:
+      #print "DEBUG skipping address %s as it's already a configured event source" %(eventsource_address)
+      continue
 
     uri='/logcollection/windows/eventsources/' + categoryName + '?msg=add&force-content-type=text/plain&expiry=600&eventsource_address=' + eventsource_address
 
@@ -251,7 +274,7 @@ def main(argv):
     urlList.append(url)
     
 
-    if not found:
+    if not grerequestsModuleFound:
       try:
         response = requests.get(url,auth=HTTPBasicAuth(user,password), verify=False, timeout=5)
         response.raise_for_status()
@@ -275,14 +298,72 @@ def main(argv):
           print "WARNING adding source " + eventsource_address + ':' + port_number + ' to ' + host + ':' + restPort + ' Event Category ' + categoryName + ': ' + response.text.rstrip()
         else:
           print "Added source " + eventsource_address + ':' + port_number + ' to ' + host + ':' + restPort + ' Event Category ' + categoryName
+      
     
-  if found:
+  if grerequestsModuleFound and len(urlList) != 0:
     print 'Using Asynchronous mode'
     try:
       reqs = (grequests.get(u, hooks = {'response' : responseHandler}, auth=HTTPBasicAuth(user,password), verify=False, timeout=5) for u in urlList)
       grequests.map(reqs,exception_handler=urlException)
     except:
       raise
+
+  if deleteOld: #remove event sources which aren't contained in the new CSV, which is treated as authoritative
+    eventSourcesToDelete = []
+    #build list of event sources to be removed
+    for source in oldEventSources:
+      if not source in newEventSources:
+        eventSourcesToDelete.append(source)
+    
+    #if we have event sources to remove, now call deleteEventSources()  
+    if len(eventSourcesToDelete) != 0:
+      #/logcollection/windows/eventsources/TEST?msg=delete&force-content-type=application/json&expiry=600&name=192.168.1.1
+      for source in eventSourcesToDelete:
+        print "Removing event source " + source + ' from ' + host + ':' + restPort + ' Event Category ' + categoryName
+        deleteEventSourceUri = '/logcollection/windows/eventsources/' + categoryName + '?msg=delete&force-content-type=application/json&expiry=600&name=' + source
+        deleteEventSourcesUrl = proto + host + ':' + restPort + deleteEventSourceUri
+        deleteEventSource(deleteEventSourcesUrl, user, password)
+
+
+def deleteEventSource(url, user, password):
+  #print "Event sources to delete:"
+  #pprint(sources)
+  try:
+    #print "trying", url
+    response = requests.get(url,auth=HTTPBasicAuth(user,password), verify=False, timeout=5)
+    response.raise_for_status()
+  except requests.exceptions.ConnectTimeout:
+    print 'Connection to ' + host + ':' + restPort + ' timed out'
+    sys.exit(1)
+  except requests.ConnectionError, e:
+    print 'ERROR connecting to host ' + host + ':' + restPort
+    print str(e)
+    sys.exit(1)
+  except requests.HTTPError, e:
+    if e.response.status_code == 401 :
+      print 'ERROR connecting to host ' + host + ':' + restPort + ': ' + str(e)
+    else:
+      print 'ERROR deleting source ' + eventsource_address + ':' + port_number  + ' to ' + host + ':' + restPort +  ' Event Category ' + categoryName + ': ' + str(e)
+    sys.exit(1)    
+
+def getExistingEventSources(url, user, password):
+  try:
+    response = requests.get(url,auth=HTTPBasicAuth(user,password), verify=False, timeout=5)
+    response.raise_for_status()
+  except requests.exceptions.ConnectTimeout:
+    print 'Connection to ' + host + ':' + restPort + ' timed out'
+    sys.exit(1)
+  except requests.ConnectionError, e:
+    print 'ERROR connecting to host ' + host + ':' + restPort
+    print str(e)
+    sys.exit(1)
+  except requests.HTTPError, e:
+    if e.response.status_code == 401 :
+      print 'ERROR connecting to host ' + host + ':' + restPort + ': ' + str(e)
+    else:
+      print 'ERROR getting event sources from ' + host + ':' + restPort + ': ' + str(e)
+    sys.exit(1)
+  return response.content
 
 
 def responseHandler(response, **kwargs):
